b70bf034475d2a219581729fd3bbb27d80690d9b
longfeizheng <longfei_zheng@163.com>
2018-01-15 21:38:03 +0800

    Initial commit

---
+++ src/main/java/cn/merryyou/security/SecurityOauth2Application.java
@@ -0,0 +1,12 @@
+package cn.merryyou.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOauth2Application.class, args);
+	}
+}
---
+++ src/main/java/cn/merryyou/security/server/MerryyouAuthorizationServerConfig.java
@@ -0,0 +1,17 @@
+package cn.merryyou.security.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created on 2018/1/15 0015.
+ *
+ * @author zlf
+ * @email i@merryyou.cn
+ * @since 1.0
+ */
+@Configuration
+@EnableAuthorizationServer
+public class MerryyouAuthorizationServerConfig {
+
+}
